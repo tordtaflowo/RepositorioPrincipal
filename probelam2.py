d3c9270ab857c4c4b7ae6c7d864f1add4cae1fbb
tordtaflowo <103199170+tordtaflowo@users.noreply.github.com>
2022-05-03 11:13:05 -0400

    Add files via upload

---
+++ probelam2.py
@@ -0,0 +1,19 @@
+nombre = "Francisco"
+adn = "NABSGHSJAKASMANAS"
+
+##melmaquiano = Extraterrestre("Francisco", "NABSGHSJAKASMANAS")
+
+class Extraterrestre:
+    def __init__(self, adn, nombre):
+        self.adn = "NABSGHSJAKASMANAS"
+        self.nombre = "Francisco"
+        self.mutacion
+
+    def Mutacion (Extraterrestre, adn):
+        if adn < 20:
+            return ("True (El ADN tiene menos de 20 letras)")
+
+
+print("El ADN tiene mutacion?: " ,Mutacion)
+
+    
